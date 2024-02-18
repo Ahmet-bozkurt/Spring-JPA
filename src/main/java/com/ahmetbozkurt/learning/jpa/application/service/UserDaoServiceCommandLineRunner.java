1deae1a4db8a5c07f309c0317829fecdc003ec4a
ahmetbozkurtt <ahmet.bozkurt@canovate.com>
2024-02-19 01:14:36 +0300

    Added logger.

--- src/main/java/com/ahmetbozkurt/learning/jpa/application/service/UserDaoServiceCommandLineRunner.java
+++ src/main/java/com/ahmetbozkurt/learning/jpa/application/service/UserDaoServiceCommandLineRunner.java
@@ -1,6 +1,8 @@
 package com.ahmetbozkurt.learning.jpa.application.service;
 
 import com.ahmetbozkurt.learning.jpa.application.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,10 +13,15 @@ public class UserDaoServiceCommandLineRunner implements CommandLineRunner {
     @Autowired
     UserDaoService userDaoService;
 
+    private final Logger log = LoggerFactory.getLogger(UserDaoServiceCommandLineRunner.class);
+
     @Override
     public void run(String... args) throws Exception {
+
         User user = new User("Cagkan", "Admin");
 
         userDaoService.insertUser(user);
+
+        log.info("New user created: " + user);
     }
 }
